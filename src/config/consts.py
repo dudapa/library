886f2d7f7dea7dc10acab3d70872a6e6ed6995a3
Patrik Duda <patrikduda94@gmail.com>
2022-03-22 18:23:26 +0100

    Add database constants

---
+++ src/config/consts.py
@@ -0,0 +1,15 @@
+import os
+
+test = 'It is just a test'
+# Table constants 
+BORROWERS_TABLE=os.getenv("BORROWERS_TABLE", "borrowers")
+AUTHORS_TABLE=os.getenv("AUTHORS_TABLE", "authors")
+GENRES_TABLE=os.getenv("GENRES_TABLE", "genres")
+BOOKS_TABLE=os.getenv("BOOKS_TABLE", "books")
+BORROWS_TABLE=os.getenv("BORROWS_TABLE", "borrows")
+
+# Database constants 
+POSTGRES_HOST=os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_DATABASE=os.getenv("POSTGRES_DATABASE", "postgres")
+POSTGRES_USER=os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD", "postgres")
