0e42a0482d75ad1974b03cbbbc4d526de39ac706
Patrik Duda <patrikduda94@gmail.com>
2022-04-12 19:16:01 +0200

    Do refactoring

--- database/create_table.sql
+++ database/create_table.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS borrowers CASCADE;
 
 CREATE TABLE borrowers (
 	borrower_id serial PRIMARY KEY,
-	name VARCHAR(128) NOT NULL,
+	firstname VARCHAR(128) NOT NULL,
 	surname VARCHAR(128) NOT NULL,
-	birtdate DATE NOT NULL,
+	birthdate DATE NOT NULL,
 	gender VARCHAR(16) NOT NULL
 );
 
@@ -13,8 +13,8 @@ DROP TABLE IF EXISTS authors CASCADE;
 
 CREATE TABLE authors (
 	author_id serial PRIMARY KEY,
-	name VARCHAR(128) NOT NULL,
-	surname VARCHAR(128) NOT NULL,
+	author_name VARCHAR(128) NOT NULL,
+	author_surname VARCHAR(128) NOT NULL,
 	birthdate DATE NOT NULL,
 	nationality VARCHAR(128) NOT NULL
 );
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS genres CASCADE;
 
 CREATE TABLE genres (
 	genre_id serial PRIMARY KEY,
-	name VARCHAR(128) NOT NULL
+	genre_name VARCHAR(128) NOT NULL
 );
 
 
@@ -32,7 +32,7 @@ DROP TABLE IF EXISTS books CASCADE;
 
 CREATE TABLE books (
 	book_id serial PRIMARY KEY,
-	name VARCHAR(128) NOT NULL,
+	book_name VARCHAR(128) NOT NULL,
 	page_count INT NOT NULL,
 	author_id INT NOT NULL,
 	genre_id INT NOT NULL,
@@ -45,11 +45,11 @@ CREATE TABLE books (
 DROP TABLE IF EXISTS borrows CASCADE;
 
 CREATE TABLE borrows (
-	borrow_id serial PRIMARY KEY,
 	borrower_id INT NOT NULL,
 	book_id INT NOT NULL,
-	borrow_date DATE NOT NULL,
+	borrow_date DATE DEFAULT CURRENT_DATE,
 	return_date DATE,
+	CONSTRAINT pk_borrower_book PRIMARY KEY (borrower_id, book_id),
 	CONSTRAINT fk_borrower FOREIGN KEY (borrower_id) REFERENCES borrowers(borrower_id) ON DELETE CASCADE ON UPDATE NO ACTION,
 	CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE ON UPDATE NO ACTION
 );
